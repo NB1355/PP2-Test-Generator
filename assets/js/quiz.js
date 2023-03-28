ac3228908726b2e40a6aa0753219e68efbffd96f
NB1355 <nahal5513@gmail.com>
2023-03-28 22:55:27 +0000

    ADD Features to choose between Exam or LEARN mode and set the time

---
+++ assets/js/quiz.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("x1-button").addEventListener("click", runTest);
+document.getElementById("x2-button").addEventListener("click", questionSubmit);
+document.getElementById("x3-button").addEventListener("click", showAnswers);
+document.getElementById("x4-button").addEventListener("click", showResult);
