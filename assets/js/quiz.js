552744c658c617bb0d4f449a03fe47da102bc000
NB1355 <nahal5513@gmail.com>
2023-03-30 21:55:13 +0000

    ADD Functions to get setup parameters Mode, Timer, Limit

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,53 @@
 
 
-document.getElementById("x1-button").addEventListener("click", runTest);
-document.getElementById("x2-button").addEventListener("click", questionSubmit);
-document.getElementById("x3-button").addEventListener("click", showAnswers);
-document.getElementById("x4-button").addEventListener("click", showResult);
+document.getElementById("btn-start").addEventListener("click", setUp);
+
+function setUp() {
+    getMode();
+    getTimer();
+    getLimit();
+}
+
+function getMode() {
+
+    document.getElementsByName("set_mode").forEach(radio => {
+        if (radio.checked) {
+            let theMode = radio.value;
+
+            // for functional test
+            console.log(radio.value);
+            document.getElementById("getMode-T").innerHTML = "=> We are in " + theMode + " mode.";
+
+        }
+    })
+}
+
+function getTimer() {
+
+    if (document.getElementById('set-timer').checked) {
+
+        let theTimer = "on"
+
+        // for functional test
+        document.getElementById("getTimer-T").innerHTML = `=> Timer is ${theTimer}`;
+        console.log(theTimer);
+    }
+    else {
+
+        let theTimer = "off"
+
+        // for functional test
+        document.getElementById("getTimer-T").innerHTML = `=> Timer is ${theTimer}`;
+        console.log(theTimer);
+    }
+}
+
+function getLimit() {
+
+    let theLimit = document.getElementById('set-limit').value;
+
+    // for functional test
+    document.getElementById("getLimit-T").innerHTML = `=> Time limit is ${theLimit}`;
+    console.log(theLimit);
+
+}
